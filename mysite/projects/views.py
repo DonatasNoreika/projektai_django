2f60be365e5b807233de142b166cdd1b16b5c221
Donoras <donoras@gmail.com>
2021-05-06 11:11:10 +0300

    Added project list

--- mysite/projects/views.py
+++ mysite/projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Project
 
 # Create your views here.
+
+class ProjectListView(generic.ListView):
+    model = Project
+    template_name = 'project_list.html'
